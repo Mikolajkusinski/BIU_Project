48c46b1bfd4c8e0f04d2f69571d55c6bc20133b3
Mikolajkusinski <77103765+Mikolajkusinski@users.noreply.github.com>
2023-04-09 19:34:41 +0200

    Added map + working on user location

--- project/src/map/googleMap.js
+++ project/src/map/googleMap.js
@@ -1,17 +1,40 @@
-import {useJsApiLoader, GoogleMap} from '@react-google-maps/api'
+import {useLoadScript, GoogleMap} from '@react-google-maps/api';
+import React, { useState,useEffect } from 'react';
 
+
+import styles from "./googleMap.module.css";
 
 function Map(){
-    const {isLoaded} = useJsApiLoader({googleMapsApiKey: process.env.GOOGLE_API_KEY,})
+    const [location, setLocation] = useState({});
+    const {isLoaded} = useLoadScript({googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,});
+
+    function getLocation(){
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition((position) => {
+            const {latitude, longitude} = position.coords;
+            setLocation(latitude, longitude);
+          }, (error) => {
+            console.log(error);
+          });
+        } else {
+          console.log("Geolocation is not supported by this browser.");
+        }
+    }
+    
+    useEffect(() => {
+        getLocation();
+      }, []
+    );
+
+    
 
     if(!isLoaded){
-        return <SkeletonText/>
+        return (<div>Loading...</div>);
     }
+    console.log(location.latitude);
+    return(
+        <GoogleMap zoom={10} center={{lat: location.latitude, lng: location.longitude}} mapContainerClassName={styles.map}></GoogleMap>
 
-    retrun(
-    <div>
-        <GoogleMap/>
-    </div>
     );
 }
 
